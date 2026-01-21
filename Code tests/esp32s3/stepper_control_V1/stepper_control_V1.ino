5924ca43b0012a5294da78baf4e85b844402dfa9
Petar Georgiev <130911725+figit3129bg@users.noreply.github.com>
2026-01-22 00:04:28 +0200

    Add files via upload

--- Code tests/esp32s3/stepper_control_V1/stepper_control_V1.ino
+++ Code tests/esp32s3/stepper_control_V1/stepper_control_V1.ino
@@ -4,6 +4,8 @@
 // MS1, MS2 and MS3 pins are all set to HIGH and its hardwired into the solder board.
 // All of them are conected to 5V
 
+#include <AccelStepper.h>
+
 #define step1Pin 9      //GPIO 9
 #define dir1Pin 10      //GPIO 10
 #define step2Pin 11     //GPIO 11
@@ -14,8 +16,58 @@
 #define panLimitSwitchL 17
 #define panLimitSwitchR 18
 
+long maxPanPos = 0;
+long maxTiltPos = 50000;
+long leftSteps = 0;
+long rightSteps = 0;
+long panCenterPosition = 0;
+long tiltCurentPosition = 0;
+
+float maxSpeed = 5000; //max speed in steps
+
+AccelStepper stepperPan(AccelStepper::DRIVER, step1Pin, dir1Pin);
+AccelStepper stepperTilt(AccelStepper::DRIVER, step2Pin, dir2Pin);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  pinMode(panLimitSwitchL, INPUT_PULLUP);
+  pinMode(panLimitSwitchR, INPUT_PULLUP);
+  pinMode(tiltLimitSwitch, INPUT_PULLUP);
+  
+  stepperPan.setMaxSpeed(5000);
+  stepperPan.setAcceleration(8000);
+  stepperTilt.setMaxSpeed(5000);
+  stepperTilt.setAcceleration(8000);
+
+  // -----Homing-----
+  // Rotate left until switch pressed
+  stepperPan.setSpeed(-3000);
+  while (digitalRead(panLimitSwitchL) == HIGH) stepperPan.runSpeed();
+  steppePan.setCurrentPosition(0);
+  leftSteps = 0;
+  Serial.println("Left homed");
+  delay(250);
+
+  // Rotate right until switxch pressed
+  stepperPan.setSpeed(3000);
+  while (digitalRead(panLimitSwitchR) == HIGH) stepperPan.runSpeed();
+
+  // Record max pan position
+  rightSteps = stepperPan.currentPosition();
+  maxPanPos = rightSteps;
+  Serial.print("Right Steps: ");
+  Serial.println(rightSteps);      
+      
+  // Centering the Z axis
+  panCenterPosition = maxPanPos/2;
+  stepperPan.moveTo(panCenterPosition);
+  stepperPan.runToPosition();
+
+  // Tilt homing 
+  stepperTilt.setSPeed(-3000);
+  while (digitalRead(tiltLimitSwitch) == HIGH) stepperTilt.runSpeed();
+  stepperTilt.setCurrentPosition(0);    
 
 }
 
